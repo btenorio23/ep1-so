84269d91a0a7098d59a239843b4c5b4d9274508f
btenorio23 <bruno-tenorio23@hotmail.com>
2016-10-09 19:56:31 -0300

    Atualização

--- main/Main.java
+++ main/Main.java
@@ -1,30 +1,10 @@
-package main;
 
-import core.Maquina;
-import escalonador.TabelaProcessosGUI;
-
-//Classe responsável por iniciar o programa, mostrar todos os arquivos que devem ser lidos, etc
 public class Main {
 
 	public static void main(String[] args) {
-		Maquina maquina = new Maquina();
-		maquina.iniciaMaquina();
-		
-		/*
-		TabelaProcessos table = new TabelaProcessos();
-		table.criaTabela();
-		try {
-			while(true) {
-				Thread.sleep(6000);
-				maquina.devolveProcessos();
-				System.out.println("Devolvendo processos");
-			}
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		*/
-		
+		Escalonador escalonador = new Escalonador();
+		escalonador.run();
+
 	}
-	
+
 }
